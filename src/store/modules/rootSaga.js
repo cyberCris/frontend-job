8b335af843d3632423ca6593bf68030f29e4dab5
Cristiano Soares <crssoares.js@gmail.com>
2020-06-02 08:20:34 -0300

    reducer fix heroku

--- src/store/modules/rootSaga.js
+++ src/store/modules/rootSaga.js
@@ -1,5 +1,5 @@
 import { all } from 'redux-saga/effects';
-import user from './user/sagas';
+import user from '~store/modules/user/sagas';
 
 export default function* rootSaga() {
   return yield all([user]);
